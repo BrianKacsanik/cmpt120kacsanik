1de8b117499ee85d2982b2434d86d5c950216dbd
Marist User <brian.kacsanik@gmail.com>
2019-02-11 17:15:56 -0500

    Solution to Curiosity rover exercise

---
+++ rover.py
@@ -0,0 +1,9 @@
+# rover.py
+# Create a program that tells how long for the Mars rover to send an image to Earth.
+
+def main ():
+    distance = 34000000
+    speed = 186000
+    print ("It takes", distance / speed, "seconds for the Mars rover to send an image to Earth")
+
+main()

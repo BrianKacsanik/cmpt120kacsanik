4120b0516fb22f1399c053430307532f86c342f7
Marist User <brian.kacsanik1@marist.edu>
2019-02-11 17:47:09 -0500

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,11 @@
+# madlib.py
+# Create a program that uses user inputed words to create a sentence
+
+def main():
+    noun = input("Enter a noun")
+    verb = input("Enter a verb")
+    adjective = input("Enter a adjective")
+    place = input("Enter a place")
+    print ("Take your", adjective, noun, "and", verb, "it at the", place, "!")
+
+main()

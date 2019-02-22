a26d8b8d1bf0cc5f70612872ded3d19959776150
BrianKacsanik <47280608+BrianKacsanik@users.noreply.github.com>
2019-02-22 17:02:06 -0500

    Add files via upload

---
+++ pi.py
@@ -0,0 +1,13 @@
+# pi.py
+# Create a program that approximates the value of pi.
+
+import math
+
+def main():
+    n =eval(input("Enter a number"))
+    approx = 0
+    for i in range(n):
+        approx = approx + 4 * (-1) ** i / (2 * i + 1)
+    print(math.pi - approx)
+
+main()

b572ba6ebfc587a60e65e3a932ccb11c24584d46
Brian Kacsanik <brian.kacsanik1@marist.edu>
2019-02-25 18:12:23 -0500

    Finished part two of lab four

---
+++ credentials2.py
@@ -0,0 +1,22 @@
+def getname():
+    first = input("Enter your first name: ")
+    last = input("Enter your last name: ")
+    return [first, last]
+
+name = getname()
+
+def username():
+    return name[0] + "." + name [1]
+
+uname = username()
+
+def password():
+    passwd = input("Create a new password: ")
+    characters = len(passwd)
+    if characters < 8:
+        print("Fool of a Took! That password is feeble!")
+        passwd = input("Create a new password: ")
+    return [passwd]
+
+password()
+print("Account configured. Your new email address is", uname + "@marist.edu")

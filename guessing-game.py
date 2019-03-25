a0f91c566595f31515b350751b0b441dfc1d5cc7
Brian Kacsanik <brian.kacsanik1@marist.edu>
2019-03-25 18:11:49 -0400

    Usability improvements to the guessing game, resolves #4 and resolves #5.

--- guessing-game.py
+++ guessing-game.py
@@ -10,12 +10,18 @@ def main():
         animalguess = str.lower(input())
         if animalguess == animal:
             print("Congratulations, you win!")
+            print("Do you like this animal? (y/n)")
+            animalaproval = input()
+            if animalaproval == "y":
+                print("Good for you")
+            elif animalaproval == "n":
+                print("That's disapointing")
             animalguess = "correct"
-        elif animalguess == "end":
+        elif animalguess[0] == "q":
             animalguess = "quit"
         else:
             print("Incorrect, try again")
-            print("Type 'end' to exit")
+            print("Type something that starts with the letter q to exit")
             animalguess = "incorrect"
 
 main()
